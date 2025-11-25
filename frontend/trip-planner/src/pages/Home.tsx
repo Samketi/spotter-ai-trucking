8192660ceab83cc9c8e9386af21252e77d5a3c6b
Samketi Nkosi <samketinkosi@Samketis-MacBook-Pro.local>
2025-11-25 15:33:57 +0200

    fix there and there

--- frontend/trip-planner/src/pages/Home.tsx
+++ frontend/trip-planner/src/pages/Home.tsx
@@ -1,17 +1,5 @@
-import { useState } from "react";
 import TripForm from "../components/TripForm";
-
-import { useNavigate } from "react-router-dom";
-import type { TripResponse } from "../types/trip";
-
 export default function Home() {
-  const [trip, setTrip] = useState<TripResponse | null>(null);
-  const navigate = useNavigate();
-
-  const handleTripCreated = (newTrip: TripResponse) => {
-    setTrip(newTrip);
-    navigate(`/trip/${newTrip.trip_id}`);
-  };
 
   return (
     <div className="p-6 max-w-3xl mx-auto">
